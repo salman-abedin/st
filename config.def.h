319bdb1a1d741869fd92e734512c82f6ddc70efe
salman-abedin <salman104alvi@gmail.com>
2020-09-18 18:18:31 +0600

    added secondary fonts

--- config.def.h
+++ config.def.h
@@ -5,7 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "mono:style=bold:size=15:antialias=true:autohint=true";
+/* static char font[] = "mono:style=bold:size=15:antialias=true:autohint=true";
+ */
+static char font[] = "mono:size=15:antialias=true:autohint=true";
 
 /* Spare fonts */
 static char *font2[] = {
@@ -113,7 +115,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.85;
+float alpha = 0.90;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
